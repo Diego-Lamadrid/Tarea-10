156e94df0bc53d3a80b0bc13ee871bbe9dca860a
Diego-Lamadrid <112910313+Diego-Lamadrid@users.noreply.github.com>
2022-09-10 21:57:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let edad;
+edad = prompt("introduce tu edad");
+if (edad >= 18){
+    alert("Puedes entrar, eres mayor de edad.");
+    } else {
+        alert("NO pedes entrar eres menor de edad");
+    }
